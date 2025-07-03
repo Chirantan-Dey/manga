0999afffa0eef08099462da5250790ef36933e7e
Chirantan Dey <chiru.d.guy@gmail.com>
2025-07-03 16:38:11 +0000

    script running

--- Suwayomi-Server/server/src/main/kotlin/suwayomi/tachidesk/graphql/mutations/ThumbnailMutation.kt
+++ Suwayomi-Server/server/src/main/kotlin/suwayomi/tachidesk/graphql/mutations/ThumbnailMutation.kt
@@ -1,23 +1,28 @@
 package suwayomi.tachidesk.graphql.mutations
 
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import graphql.schema.DataFetchingEnvironment
+import io.javalin.http.UploadedFile
+import org.koin.core.Koin
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import suwayomi.tachidesk.manga.impl.ThumbnailDownloadHelper
-import java.io.InputStream
 
 class ThumbnailMutation : KoinComponent {
     private val thumbnailHelper: ThumbnailDownloadHelper by inject()
 
+    @GraphQLIgnore
+    override fun getKoin(): Koin = super.getKoin()
+
     /**
      * Sets a custom thumbnail for a manga.
      * @param mangaId The ID of the manga.
-     * @param thumbnail The input stream of the thumbnail image.
+     * @param thumbnail The uploaded thumbnail file.
      * @param env The DataFetchingEnvironment.
      * @return True if the thumbnail was set successfully, false otherwise.
      */
-    fun setCustomThumbnail(mangaId: Int, thumbnail: InputStream, env: DataFetchingEnvironment): Boolean {
-        thumbnailHelper.saveCustomThumbnail(mangaId, thumbnail)
+    fun setCustomThumbnail(mangaId: Int, thumbnail: UploadedFile, env: DataFetchingEnvironment): Boolean {
+        thumbnailHelper.saveCustomThumbnail(mangaId, thumbnail.content())
         return true // Assuming saveCustomThumbnail is synchronous or we return immediately
     }
 
